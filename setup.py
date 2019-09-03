d547816d40a6a3cffaffbe9bf468b4d52e98c428
David James Skelton <james.skelton3@googlemail.com>
2019-09-03 17:59:04 +0200

    Updated with files so that the project could be submitted on PyPI.

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
 import setuptools
 
+# Read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="mcpt",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python library for calculating p-values using Monte Carlo sampling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/ravenlocke/mcpt",
     author="David J. Skelton",
-    licence="MIT",
+    author_email="d.j.skelton1@gmail.com",
+    license="MIT",
     packages=["mcpt"],
     install_requires=["scipy", "numpy"],
     zip_safe=False,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ]
 )
